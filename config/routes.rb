fdf407747fd2f641cd80f6c04e2a1bb1b2935c86
Clinton Pham <clintonjob@hotmail.co.uk>
2021-04-23 08:55:53 +0000

    Started on nav bar. Works as intended, need to implement the pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # get 'home/index'
+  
+  # get 'home/about'
+  # Default Page
+  root 'home#index' 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
