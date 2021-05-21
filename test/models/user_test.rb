a4bb4da0c7e81f6b9a2a89e141a2c86b2f825048
sean8900 <s3806690@student.rmit.edu.au>
2021-05-21 04:40:57 +0000

    heroku

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ class UserTest < ActiveSupport::TestCase
    
 
   def setup
-    @user = User.new(name: "test10", email: "test10@hotmail.com")
+    @user = User.new(name: "", email: "test10@hotmail.com")
   end 
   
   test "name should be present" do
@@ -14,7 +14,7 @@ class UserTest < ActiveSupport::TestCase
   
   test "username should not be too long" do 
     @user.name = "x" * 26
-    assert @user.valid?
+    assert_not @user.valid?
   end
    
   # test "should be valid" do
